4084ad96369e379e69c41891b04cbdac41501daa
Musty-Mode <mustafayare1001@gmail.com>
2022-12-08 08:48:48 -0800

    w

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-int main (void)
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	int a;
 	long int b;
@@ -13,5 +16,4 @@ int main (void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
-
 }
