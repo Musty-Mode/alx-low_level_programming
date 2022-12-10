a79ddbeee90b3e0891d20c3a2f0448a48ac3dd09
Musty-Mode <mustafayare1001@gmail.com>
2022-12-10 06:12:47 -0800

    QA review

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,8 +4,8 @@
 
 	/**
 	 * main -Prints the last digit of a randomly generated number
-	 * 	and whether it is greater than 5, less than 6, or 0.
-	 * 
+	 *	and whether it is greater than 5, less than 6, or 0.
+	 *
 	 * Return: 0 on success.
 	 */
 int main(void)
