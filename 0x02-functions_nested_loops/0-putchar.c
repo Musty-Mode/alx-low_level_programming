a53244e14a6397f3cbc7fe6fb2116cb3af239d38
Musty-Mode <mustafayare1001@gmail.com>
2022-12-13 04:20:40 -0800

    hardest work yet

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - prints _putchar, followed by a new line.
+ *
+ * Return: 0 Success
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(c[i]);
+	}
+
+	_putchar('\n');
+	return (0);
+}
